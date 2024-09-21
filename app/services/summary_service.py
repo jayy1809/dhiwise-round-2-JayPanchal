1135241b19851bd29e87dd3c7090303fae2061cb
Jay Panchal <jaypanchal1809@gmail.com>
2024-09-21 23:00:22 +0530

    added try catch in summary_service

--- app/services/summary_service.py
+++ app/services/summary_service.py
@@ -14,28 +14,33 @@ from app.utils.video_processing import process_video_transcription
 
 
 def gen_summary(agenda, index_name, namespace):
-    query = SUMMARY_TRANSCRIPT_QUERY
-    system_prompt = SUMMARY_SYSTEM_PROMPT
+    
+    try:
+        query = SUMMARY_TRANSCRIPT_QUERY
+        system_prompt = SUMMARY_SYSTEM_PROMPT
 
-    file_name = ""
-    for file_name_details in os.listdir(LOADING_DIRECTORY):
-        if file_name_details.endswith(".mp4"):
-            file_name = file_name_details
+        file_name = ""
+        for file_name_details in os.listdir(LOADING_DIRECTORY):
+            if file_name_details.endswith(".mp4"):
+                file_name = file_name_details
 
-    transcript = process_video_transcription(f"./data/{file_name}")
+        transcript = process_video_transcription(f"./data/{file_name}")
 
-    vectors = chunk_and_embed(text=transcript)
+        vectors = chunk_and_embed(text=transcript)
 
-    upsert_vectors(index_name=index_name, namespace=namespace, vectors=vectors)
+        upsert_vectors(index_name=index_name, namespace=namespace, vectors=vectors)
 
-    nodes = query_data(
-        index_name=index_name, namespace=namespace, query=query, k_value=7
-    )
+        nodes = query_data(
+            index_name=index_name, namespace=namespace, query=query, k_value=7
+        )
 
-    context = "\n".join([node.text for node in nodes])
+        context = "\n".join([node.text for node in nodes])
 
-    user_prompt = summary_user_prompt(transcript_data=context, agenda=agenda)
+        user_prompt = summary_user_prompt(transcript_data=context, agenda=agenda)
 
-    summary = get_chat_completion(user_prompt, system_prompt)
+        summary = get_chat_completion(user_prompt, system_prompt)
 
-    return summary
+        return summary
+    
+    except Exception as e:
+        raise e
